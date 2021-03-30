e3ddefd3e0db27d4bd4fc8937a068d175ba8c01d
Chema Santos <jose.santos@grupoleitz.com>
2021-03-30 09:51:46 -0600

    chema|20210330:agregando cards donde iran las tablas del dashboard

--- src/app/components/dashboard/dashboard-card/dashboard-card.component.ts
+++ src/app/components/dashboard/dashboard-card/dashboard-card.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-card',
   templateUrl: './dashboard-card.component.html',
-  styleUrls: ['./dashboard-card.component.sass']
+  styleUrls: ['./dashboard-card.component.sass'],
 })
 export class DashboardCardComponent implements OnInit {
+  // @ts-ignore
+  @Input() imgSrc: string;
+  // @ts-ignore
+  @Input() description: string;
+  // @ts-ignore
+  @Input() quantity: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
