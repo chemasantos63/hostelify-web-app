beb70f89690a1029d49b56cdc70e1f9f8e26f09e
Chema Santos <jose.santos@grupoleitz.com>
2021-06-26 20:31:37 -0600

    chema|20210626:agregando variables con regex para validar numeros

--- src/app/shared/utils.ts
+++ src/app/shared/utils.ts
@@ -3,3 +3,7 @@ import { Reservation } from "../components/reservation/reservation.component";
 export const getRoomsNumber = (reservation: Reservation) => {
   return reservation.rooms.reduce((acc, act) => `${acc}${act.roomNumber},`, ``);
 };
+
+export const regexForNumbersWithDecimal = /^[0-9]\d*(\.\d{1,4})?$/;
+
+export const regexJustForNumbersWithoutDecimal = /^[0-9]\d*$/;
