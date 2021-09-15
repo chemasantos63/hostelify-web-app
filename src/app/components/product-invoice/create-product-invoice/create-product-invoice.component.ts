524d00f94460b0e91d8b8f8bd18b59111cfaa229
JS2558 <jisai2558@gmail.com>
2021-09-14 20:02:38 -0600

    tabla detalle factura productos

--- src/app/components/product-invoice/create-product-invoice/create-product-invoice.component.ts
+++ src/app/components/product-invoice/create-product-invoice/create-product-invoice.component.ts
@@ -64,7 +64,7 @@ export class CreateProductInvoiceComponent implements OnInit {
     console.log(product);
 
     this.productsToInvioceDataSource.data.push(product);
-
+    
     console.log(this.productsToInvioceDataSource);
   }
 }
